e7173557e6c72c0704e5db3288b8d213a35def7f
Marc Sladek <marc.sladek@synventis.com>
2019-05-14 15:51:51 +0200

    add comment

--- maven/prepare-release.sh
+++ maven/prepare-release.sh
@@ -20,9 +20,10 @@ echo "... done" || die "unable to prepare branch ${BRANCH}"
 
 echo
 echo "Updating pom.xml using latest releases ..."
+# doesn't work for milestones with naming schema 'x.y-M1'
 mvn versions:use-releases \
     -Dincludes=com.celements:*,ch.programmonline:*,ch.newjobplacement:* \
-    -DprocessParent=true -DgenerateBackupPoms=false && \
+    -DprocessParent=true -DfailIfNotReplaced=true -DgenerateBackupPoms=false && \
 echo "... done" || die "maven versions command failed"
 
 while :
