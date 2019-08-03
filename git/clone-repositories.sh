07ff2617182bff11ff816a4562f7d0483bf1d50f
Marc Sladek <marc.sladek@synventis.com>
2019-08-03 19:12:05 +0200

    git clone all repos (#7)
    * clone-repositories.sh
    
    * fix var name

---
+++ git/clone-repositories.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Enter GitHub username: " username
+for project in celements synventis programmonline; do
+  echo "Fetching $project repositories..."
+  curl -s -u $username "https://api.github.com/orgs/$project/repos?page=1&per_page=100" | \
+    grep "ssh_url" | awk '{print $2}' | sed -e 's:\"::g' -e 's:,$::' >> repo-list.txt
+done
+read -p "[ENTER] to start cloning. Change local repo names by adding it to the line in repo-list.txt"
+cat repo-list.txt | while read url; do git clone $url; done
