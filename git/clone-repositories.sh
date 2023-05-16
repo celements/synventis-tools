d9ca6192571e769f7fdebdf769c3e0e94e30c3ef
Fabian Pichler <fpichler@tmp.srv-inter.net>
2023-05-16 23:14:51 +0200

    add support for bearerToken

--- git/clone-repositories.sh
+++ git/clone-repositories.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-read -p "Enter GitHub username: " username
+read -p "Enter GitHub bearer token: " bearerToken
 for project in celements synventis programmonline; do
   echo "Fetching $project repositories..."
-  curl -s -u $username "https://api.github.com/orgs/$project/repos?page=1&per_page=100" | \
+  curl -s --header "Authorization: Bearer $bearerToken"\
+   --header "X-GitHub-Api-Version: 2022-11-28"\
+   "https://api.github.com/orgs/$project/repos?page=1&per_page=100" | \
     grep "ssh_url" | awk '{print $2}' | sed -e 's:\"::g' -e 's:,$::' >> repo-list.txt
 done
 read -p "[ENTER] to start cloning. Change local repo names by adding it to the line in repo-list.txt"
