7529252aa999f64a59ef981d3699f3670b9acdd5
Marc Sladek <marc@sladek.dev>
2024-05-11 00:20:26 +0200

    improve pull-and-clean branch handling (#19)
    * improve pull-and-clean
    
    * join order

--- git/pull-and-clean.sh
+++ git/pull-and-clean.sh
@@ -2,6 +2,8 @@
 ## Marc Sladek <marc@sadek.me>, June-Nov 2018
 ## pulls and cleans the branches of the current or given and sub git repositories in parallel
 
+baseBranches='dev|develop|master|main'
+
 main() {
   mainDir=$1
   if [ -z "$mainDir" ]; then 
@@ -17,14 +19,12 @@ main() {
       && ! ssh-add -l | grep -q "$(ssh-keygen -lf "$githubKeyfile" | awk '{print $2}')" \
       && ssh-add "$githubKeyfile"
     declare -A pids
-    runAsync "$mainDir"
-    for subDir in "$mainDir"/*; do
-      runAsync "$subDir"
+    directories=("$mainDir" "$mainDir"/*)
+    for dir in "${directories[@]}"; do
+      runAsync "$dir"
     done
-    for name in "${!pids[@]}"; do
-      wait "${pids[$name]}" \
-        && echo "done    [$name]" \
-        || echo "FAILED  [$name]"
+    for dir in "${directories[@]}"; do
+      joinAsync "$dir"
     done
     exit 0
   fi
@@ -33,24 +33,38 @@ main() {
 ## runs the script async for a given git repository '$1'
 runAsync() {
   if [ "$1" ] && [ -d "$1/.git" ]; then
-    cleanBranches "$1" dev &
+    cleanBranches "$1" &
     pids[$(basename "$1")]=$!
   fi
 }
 
+## joins the async process for a given git repository '$1'
+joinAsync() {
+  name=$(basename "$1")
+  pid=${pids[$name]}
+  [ -z "$pid" ] && return
+  wait "$pid" \
+    && echo "done    [$name]" \
+    || echo "FAILED  [$name]"
+}
+
 ## does the following process for for a git repository '$1':
-## 1. checkout branch '$2'
+## 1. tries switching to any base branch
 ## 2. pull the remote repository
 ## 3. pruning remote branches from 'origin'
 ## 4. deleting local branches gone from remote
 cleanBranches() {
-  [ "$1" ] && [ -d "$1/.git" ] && [ "$2" ] \
-    && git -C "$1" checkout "$2" > /dev/null 2>&1 \
-    && git -C "$1" pull          > /dev/null 2>&1 \
-    && git -C "$1" fetch origin --prune \
-    && git -C "$1" branch -vv | grep -E '\[origin/.*: gone\]' \
-       | awk '{print $1}' | grep -Ev "(^\*|dev|master)" \
-       | xargs --no-run-if-empty git -C "$1" branch --delete --force
+  IFS='|'
+  for b in $baseBranches; do
+    [ "$1" ] && [ -d "$1/.git" ] && [ "$b" ] \
+      && git -C "$1" switch "$b" > /dev/null 2>&1 \
+      && git -C "$1" pull        > /dev/null 2>&1 \
+      && git -C "$1" fetch origin --prune \
+      && git -C "$1" branch -vv | grep -E '\[origin/.*: gone\]' \
+        | awk '{print $1}' | grep -Ev "(^\*|${baseBranches})" \
+        | xargs --no-run-if-empty git -C "$1" branch --delete --force \
+      && return 0
+  done
   return $?
 }
 
